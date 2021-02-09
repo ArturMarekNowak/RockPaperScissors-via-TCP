c46b806bb1ba62956269641378daa521e0f39052
Artur Nowak <arturmareknowal@gmail.com>
2021-02-09 18:08:54 +0100

    binded GUI labels with ViewModel

---
+++ RockPaperScissorsTCP/Model/Game.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace RockPaperScissorsTCP.Model
+{
+    public class Game : INotifyPropertyChanged
+    {
+        private int _scoreAI = 0;
+        private int _scorePlayer = 0;
+        private string _lastResult = "-";
+        private string _addresIpV4 = "127.0.0.1";
+        private int _port = 8080;
+        public int scoreAI
+        {
+            get
+            {
+                return _scoreAI;
+            }
+            set
+            {
+                _scoreAI = value;
+                OnPropertyChanged("scoreAI");
+            }
+        }
+ 
+        public int scorePlayer
+        {
+            get
+            {
+                return _scoreAI;
+            }
+            set
+            {
+                _scoreAI = value;
+                OnPropertyChanged("scorePlayer");
+            }
+        }
+
+        public string lastResult
+        {
+            get
+            {
+                return _lastResult;
+            }
+            set
+            {
+                _lastResult = value;
+                OnPropertyChanged("lastResult");
+            }
+        }
+
+        public string addresIpV4
+        {
+            get
+            {
+                return _addresIpV4;
+            }
+            set
+            {
+                _addresIpV4 = value;
+                OnPropertyChanged("addresIpV4");
+            }
+        }
+
+        public int port
+        {
+            get
+            {
+                return _port;
+            }
+            set
+            {
+                _port = value;
+                OnPropertyChanged("port");
+            }
+        }
+
+        /// <summary>
+        /// Initializes new instance of Game class
+        /// </summary>
+        public Game(int o1, int o2, int o3, int o4, int port)
+        {
+            _scoreAI = 0;
+            _scorePlayer = 0;
+            _lastResult = "-";
+            _addresIpV4 = o1.ToString() + "." + o2.ToString() + "." + o3.ToString() + "." + o4.ToString();
+            _port = port;
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ RockPaperScissorsTCP/ViewModel/GameViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RockPaperScissorsTCP.Model;
+using System.Diagnostics;
+
+
+namespace RockPaperScissorsTCP.ViewModel
+{
+    public class GameViewModel
+    {
+        private Game _Game;
+
+        ///<summary>
+        /// Initializes new instance of Game class
+        ///</summary>
+        public GameViewModel() {
+            _Game = new Game(127, 0, 0, 1, 8080);
+        }
+
+        public Game Game
+        {
+            get
+            {
+                return _Game;
+            }
+        }
+
+        /// <summary>
+        /// Saves changes
+        /// </summary>
+
+        public void SaveChanges()
+        {
+            Debug.Assert(false, String.Format("{0} was updated", Game.scoreAI.ToString()));
+        }
+    }
+}
